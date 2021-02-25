42b3e84a3232a2bdd5c1bf989fd4ece30e630378
Pavlo Kerestey <pavlo.kerestey@thoughtworks.com>
2021-02-25 12:33:35 +0100

    Story #10: Add Alerts for outdated files on HDFS and Failed Apps to Dashboard
    Co-authored-by: Pablo Porto <pablo.porto@thoughtworks.com>

--- components/monitoring_dashboard/main.tf
+++ components/monitoring_dashboard/main.tf
@@ -48,11 +48,11 @@ resource "aws_cloudwatch_dashboard" "main" {
             "height": 3,
             "properties": {
                 "metrics": [
-                    [ "System/Linux", "MemoryUtilization", "InstanceId", "${data.terraform_remote_state.training_kafka.kafka_instance_id}", { "label": "MemoryUtilization" } ],
-                    [ ".", "DiskSpaceUtilization", "MountPath", "/", "InstanceId", "${data.terraform_remote_state.training_kafka.kafka_instance_id}", "Filesystem", "/dev/xvda1" ]
+                    [ "System/Linux", "MemoryUtilization", "InstanceId", "i-0c8ac9ba08afb5956", { "label": "MemoryUtilization" } ],
+                    [ ".", "DiskSpaceUtilization", "MountPath", "/", "InstanceId", "i-0c8ac9ba08afb5956", "Filesystem", "/dev/xvda1" ]
                 ],
                 "view": "singleValue",
-                "region": "${var.aws_region}",
+                "region": "eu-central-1",
                 "title": "Kafka",
                 "period": 300
             }
@@ -60,19 +60,19 @@ resource "aws_cloudwatch_dashboard" "main" {
         {
             "type": "metric",
             "x": 12,
-            "y": 3,
+            "y": 9,
             "width": 12,
             "height": 9,
             "properties": {
                 "metrics": [
-                    [ "AWS/ElasticMapReduce", "HDFSUtilization", "JobFlowId", "${data.terraform_remote_state.training_emr_cluster.emr_cluster_id}" ],
+                    [ "AWS/ElasticMapReduce", "HDFSUtilization", "JobFlowId", "j-3AZII9CYEZMWB" ],
                     [ ".", "AppsRunning", ".", "." ],
                     [ ".", "AppsPending", ".", "." ],
                     [ ".", "YARNMemoryAvailablePercentage", ".", "." ]
                 ],
                 "view": "singleValue",
                 "stacked": false,
-                "region": "${var.aws_region}",
+                "region": "eu-central-1",
                 "period": 300,
                 "title": "EMR HDFS"
             }
@@ -86,13 +86,13 @@ resource "aws_cloudwatch_dashboard" "main" {
             "properties": {
                 "metrics": [
                     [ { "expression": "m2+m3", "label": "MemoryUsed", "id": "e1", "color": "#d62728" } ],
-                    [ "AWS/ElasticMapReduce", "MemoryTotalMB", "JobFlowId", "${data.terraform_remote_state.training_emr_cluster.emr_cluster_id}", { "id": "m1", "color": "#1f77b4" } ],
+                    [ "AWS/ElasticMapReduce", "MemoryTotalMB", "JobFlowId", "j-3AZII9CYEZMWB", { "id": "m1", "color": "#1f77b4" } ],
                     [ ".", "MemoryReservedMB", ".", ".", { "id": "m2", "color": "#bcbd22" } ],
                     [ ".", "MemoryAllocatedMB", ".", ".", { "id": "m3", "color": "#ff7f0e" } ]
                 ],
                 "view": "timeSeries",
                 "stacked": false,
-                "region": "${var.aws_region}",
+                "region": "eu-central-1",
                 "period": 300,
                 "title": "EMR Cluster Memory Usage"
             }
@@ -100,17 +100,17 @@ resource "aws_cloudwatch_dashboard" "main" {
         {
             "type": "metric",
             "x": 12,
-            "y": 0,
+            "y": 6,
             "width": 12,
             "height": 3,
             "properties": {
                 "metrics": [
-                    [ "System/Linux", "DiskSpaceUtilization", "MountPath", "/", "InstanceId", "${data.terraform_remote_state.training_kafka.kafka_instance_id}", "Filesystem", "/dev/xvda1", { "label": "DiskSpaceUtilization" } ],
-                    [ ".", "MemoryUtilization", "InstanceId", "${data.terraform_remote_state.training_kafka.kafka_instance_id}" ]
+                    [ "System/Linux", "DiskSpaceUtilization", "MountPath", "/", "InstanceId", "i-0c8ac9ba08afb5956", "Filesystem", "/dev/xvda1", { "label": "DiskSpaceUtilization" } ],
+                    [ ".", "MemoryUtilization", "InstanceId", "i-0c8ac9ba08afb5956" ]
                 ],
                 "view": "timeSeries",
                 "stacked": false,
-                "region": "${var.aws_region}",
+                "region": "eu-central-1",
                 "title": "Kafka Status",
                 "period": 300,
                 "yAxis": {
@@ -129,29 +129,32 @@ resource "aws_cloudwatch_dashboard" "main" {
             "height": 6,
             "properties": {
                 "metrics": [
-                    [ "AWS/ElasticMapReduce", "AppsRunning", "JobFlowId", "${data.terraform_remote_state.training_emr_cluster.emr_cluster_id}" ],
-                    [ ".", "AppsPending", ".", "." ],
-                    [ ".", "ContainerReserved", ".", "." ]
+                    [ { "expression": "(RATE(m4) + RATE(m5)) * 100", "label": "Apps Killed Or Failed", "id": "e1", "color": "#d62728", "region": "eu-central-1" } ],
+                    [ "AWS/ElasticMapReduce", "AppsRunning", "JobFlowId", "j-3AZII9CYEZMWB", { "id": "m1", "color": "#2ca02c" } ],
+                    [ ".", "AppsFailed", ".", ".", { "id": "m4", "visible": false, "color": "#9467bd" } ],
+                    [ ".", "AppsKilled", ".", ".", { "id": "m5", "visible": false, "color": "#c5b0d5" } ]
                 ],
                 "view": "timeSeries",
                 "stacked": false,
-                "region": "${var.aws_region}",
-                "period": 300
+                "region": "eu-central-1",
+                "period": 300,
+                "title": "Running Apps / Killed Or Failed",
+                "stat": "Average"
             }
         },
         {
             "type": "metric",
-            "x": 12,
-            "y": 9,
+            "x": 0,
+            "y": 15,
             "width": 12,
             "height": 3,
             "properties": {
                 "view": "timeSeries",
                 "stacked": false,
                 "metrics": [
-                    [ "System/Linux", "MemoryUtilization", "InstanceId", "${data.terraform_remote_state.ingester.ingester_instance_id}" ]
+                    [ "System/Linux", "MemoryUtilization", "InstanceId", "i-02e2f041e1a53a56f" ]
                 ],
-                "region": "${var.aws_region}",
+                "region": "eu-central-1",
                 "title": "Ingester Memory Usage",
                 "period": 300,
                 "yAxis": {
@@ -160,6 +163,38 @@ resource "aws_cloudwatch_dashboard" "main" {
                         "max": 100
                     }
                 }
+            }
+        },
+        {
+            "type": "metric",
+            "x": 12,
+            "y": 0,
+            "width": 6,
+            "height": 6,
+            "properties": {
+                "title": "KafkaDiskSpaceExceeded",
+                "annotations": {
+                    "alarms": [
+                        "arn:aws:cloudwatch:eu-central-1:640172962304:alarm:KafkaDiskSpaceExceeded"
+                    ]
+                },
+                "view": "timeSeries",
+                "stacked": false
+            }
+        },
+        {
+            "type": "alarm",
+            "x": 18,
+            "y": 0,
+            "width": 6,
+            "height": 3,
+            "properties": {
+                "title": "",
+                "alarms": [
+                    "arn:aws:cloudwatch:eu-central-1:640172962304:alarm:AppsKilledOrFailed",
+                    "arn:aws:cloudwatch:eu-central-1:640172962304:alarm:HdfsFilesTooOld",
+                    "arn:aws:cloudwatch:eu-central-1:640172962304:alarm:KafkaDiskSpaceExceeded"
+                ]
             }
         }
     ]
